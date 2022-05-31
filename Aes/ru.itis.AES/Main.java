dcf021113074e08c0b868692d074a456a8cda467
Степанищева Анастасия <74156774+Stepanishcheva@users.noreply.github.com>
2022-05-31 23:17:55 +0300

    Delete Aes/ru.itis.AES directory

--- Aes/ru.itis.AES/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.AES;
-public class Main {
-  public static void main(String[] args) throws Exception{
-    Encryptor encryptor = new Encryptor();
-    SecretGenerator secretGenerator = new SecretGenerator(encryptor);
-    byte[] secret = secretGenerator.getSecret();
-    System.out.println(new String(secret));
-  }
-}
